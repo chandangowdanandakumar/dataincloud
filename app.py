d08f694704aa4212c023306cc5737025e1d9c8e8
ASU Data Science <asudatascience@datasciencelab.3500.dhcp.asu.edu>
2022-04-28 18:04:56 -0700

    app working

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from re import template
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+
+    result = {"result":True}
+    return json.dumps(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
